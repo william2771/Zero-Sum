8e11c73ebc7cd44fd665665d50d7ab507f8c2ef3
William Liu <william.weih@gmail.com>
2011-12-30 23:45:45 -0500

    Created Test files. Updated to JUnit4. Added a dummy method to test how smart Eclipse templates are.

--- Zero-Sum/src/main/java/edu/columbia/wl2271/CodeWord.java
+++ Zero-Sum/src/main/java/edu/columbia/wl2271/CodeWord.java
@@ -2,4 +2,9 @@ package edu.columbia.wl2271;
 
 public class CodeWord {
 
+	
+	public int getCodeLength()
+	{
+		return 0;
+	}
 }
---
+++ Zero-Sum/src/test/java/edu/columbia/wl2271/ZeroSumTest.java
@@ -0,0 +1,24 @@
+package edu.columbia.wl2271;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ZeroSumTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
