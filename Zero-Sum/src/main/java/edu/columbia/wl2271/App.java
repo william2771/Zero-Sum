eeff37b617b490b1a82ebe41f53ca3117ecb77c4
William Liu <william.weih@gmail.com>
2011-12-30 23:25:18 -0500

    Initialized Maven Project

---
+++ Zero-Sum/src/main/java/edu/columbia/wl2271/App.java
@@ -0,0 +1,13 @@
+package edu.columbia.wl2271;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
