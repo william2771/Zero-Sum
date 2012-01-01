d799a6cbe44aa5b54e71482698a914ef79518ee1
William Liu <william.weih@gmail.com>
2011-12-31 23:01:57 -0500

    Implemented FieldVector :)

--- Zero-Sum/src/main/java/edu/columbia/wl2271/FieldVector.java
+++ Zero-Sum/src/main/java/edu/columbia/wl2271/FieldVector.java
@@ -4,15 +4,36 @@ import java.lang.reflect.Constructor;
 
 public class FieldVector {
 	
-	public FieldVector() {
-		
+	public enum Bit { ONE, ZERO };
+	
+	/**
+	 * 
+	 * @param size
+	 */
+	public FieldVector(int size) {
+		vector = new boolean[size];
 	}
 	
+	public void set(int index, Bit val)	{
+		vector[index] = val == Bit.ONE?true:false;
+	}
+	
+	public void flip(int index) {
+		vector[index] = !vector[index];
+	}
+		
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
+		String out = "";
+		for(int k = 0; k < vector.length; k++) {
+			boolean val = vector[k];
+			char add;
+			add = val?'1':'0';
+			out += add;
+		}
+		return out;
 	}
 	
+	private boolean[] vector;
 	
 }
